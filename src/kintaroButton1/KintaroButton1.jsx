5156afd8057b9d5a28a3e17948694d2b6c366753
xkintaro <mustafatasal4242@gmail.com>
2025-07-14 13:39:35 +0300

    button update

--- src/kintaroButton1/KintaroButton1.jsx
+++ src/kintaroButton1/KintaroButton1.jsx
@@ -1,8 +1,8 @@
 import './kintaroButton1.css'
 
-const KintaroButton1 = ({ title, onClick }) => {
+const KintaroButton1 = ({ title, onClick, bgColor }) => {
     return (
-        <button className="kintaro-button-reset kintaro-button-1" title={title} onClick={onClick}>
+        <button className="kintaro-button-reset kintaro-button-1" title={title} onClick={onClick} style={{ background: bgColor }}>
             {title}
         </button>
     );
