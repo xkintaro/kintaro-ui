51320db7085d549b42a2a4b790895fd438760d0f
xkintaro <mustafatasal4242@gmail.com>
2025-07-14 15:52:25 +0300

    button update

--- src/kintaroModal/KintaroModal.jsx
+++ src/kintaroModal/KintaroModal.jsx
@@ -9,6 +9,19 @@ const KintaroModal = ({
     title,
 }) => {
 
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [isOpen]);
+
+
     if (!isOpen) return null;
 
     return (
