88e9ce906851825ecfa3b7377c675fa4775a9b30
xkintaro <mustafatasal4242@gmail.com>
2025-08-19 01:26:52 +0300

    css

--- src/kintaroHero1/KintaroHero1.jsx
+++ src/kintaroHero1/KintaroHero1.jsx
@@ -3,43 +3,24 @@ import './kintaroHero1.css';
 const KintaroHero1 = ({
     children,
     height,
-    backgroundImage = null
+    bgImage = null
 }) => {
     return (
-        <div className='kintaro-hero'
-            style={{ height: height }}
-        >
-            <div className="kintaro-hero-main">
-                {children}
+        <>
+            <div className="kintaro-hero"
+                style={{ height: height }}>
+                <div className="hero-main">
+                    {children}
+                </div>
+                {bgImage && (
+                    <img src={Hero} alt={Hero} className="hero-bg" />
+                )}
+                {bgImage && (
+                    <div className="hero-overlay"></div>
+                )}
             </div>
-            <div className="bubbles">
-                {[...Array(20)].map((_, i) => {
-                    const size = 5 + Math.random() * 15;
-                    return (
-                        <div
-                            key={i}
-                            className="bubble"
-                            style={{
-                                '--random-x': Math.random(),
-                                left: `${Math.random() * 100}%`,
-                                width: `${size}px`,
-                                height: `${size}px`,
-                                animationDuration: `${8 + Math.random() * 12}s`,
-                                animationDelay: `${Math.random() * 5}s`,
-                            }}
-                        />
-                    )
-                })}
-            </div>
+        </>
 
-            {backgroundImage && (
-                <>
-                    <div className="kintaro-hero-overlay"></div>
-                    <img src={backgroundImage} className="kintaro-hero-bg-image" />
-                </>
-            )}
-
-        </div>
     );
 };
 
